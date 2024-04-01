445db148ab3104d2748d9e70f13185a1a25409c3
読み上げ <67372333+readloud@users.noreply.github.com>
2024-04-01 20:51:32 +0700

    Add files via upload

--- installer.sh
+++ installer.sh
@@ -9,27 +9,28 @@ fi
 # For upgrades and sanity check, remove any existing i2p.list file
 rm -f /etc/apt/sources.list.d/i2p.list
 
-# Compile the i2p ppa
-echo "deb http://deb.i2p2.no/ unstable main" > /etc/apt/sources.list.d/i2p.list # Default config reads repos from sources.list.d
-wget https://geti2p.net/_static/i2p-debian-repo.key.asc -O /tmp/i2p-debian-repo.key.asc # Get the latest i2p repo pubkey
-apt-key add /tmp/i2p-debian-repo.key.asc # Import the key
-rm /tmp/i2p-debian-repo.key.asc # delete the temp key
-apt-get update # Update repos
-
-if [[ -n $(cat /etc/os-release |grep kali) ]]
-then
-	apt-get install libservlet3.0-java 
-	wget http://ftp.us.debian.org/debian/pool/main/j/jetty8/libjetty8-java_8.1.16-4_all.deb
-	dpkg -i libjetty8-java_8.1.16-4_all.deb # This should succeed without error
-	apt-get install libecj-java libgetopt-java libservlet3.0-java glassfish-javaee ttf-dejavu i2p i2p-router libjbigi-jni #installs i2p and other dependencies
-	apt-get -f install # resolves anything else in a broken state
+# Install gnupg if not installed
+if ! command -v gpg; then
+      apt-get update
+      apt-get install -y gnupg
 fi
 
-apt-get install -y i2p-keyring #this will ensure you get updates to the repository's GPG key
-apt-get install -y secure-delete tor i2p # install dependencies, just in case
+# Compile the i2p ppa
+echo "deb https://ppa.launchpadcontent.net/i2p-maintainers/i2p/ubuntu noble main" > /etc/apt/sources.list.d/i2p.list # Default config reads repos from sources.list.d
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys AB9660B9EB2CC88B  # Add i2p maintainer keys # TODO: Is there a more universal way to do this?
+apt-get update # Update repos
+
+apt-get install -y secure-delete tor i2p  i2p-router # install dependencies, just in case
 
 # Configure and install the .deb
 dpkg-deb -b kali-anonsurf-deb-src/ kali-anonsurf.deb # Build the deb package
 dpkg -i kali-anonsurf.deb || (apt-get -f install && dpkg -i kali-anonsurf.deb) # this will automatically install the required packages
+
+
+# Check if kali-anonsurf package is already installed
+if ! dpkg -l | grep -qw kali-anonsurf; then
+    echo "The package 'kali-anonsurf' did not install successfully."
+    exit 1
+fi
 
 exit 0
